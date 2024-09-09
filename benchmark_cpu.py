65e581d4f205796be27cfc390368bdca5f075250
DiegoBaleironPais <diegobaleiron888@gmail.com>
2024-09-09 10:12:44 +0200

    CPU Test Update
    Update with the benchmark_cpu.py program completed.

---
+++ benchmark_cpu.py
@@ -0,0 +1,38 @@
+import argparse
+import time 
+
+from multiprocessing import Pool
+
+def calculate_primes(limit):
+    primes = [1]
+    for next_number in range(2, limit):
+        next_prime = len(primes) - 1
+        while next_prime != 0 and next_number % primes[next_prime] != 0:
+            next_prime -= 1
+        if next_prime == 0: primes.append(next_number)
+          
+    print(len(primes))
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        prog="python StressTestCPU.py",
+        description=(
+            "This program stresses the CPU by calculating all prime numbers up to a specified limit, "
+            "the program can be configurated to repeat the function in different cores to increase the stress."
+        )
+    )
+    
+    arg_parser.add_argument('upper_limit', type=int, help="The upper limit (inclusive) for the prime number search.")
+    arg_parser.add_argument('-c', '--cores', type=int, help="Number of process that will repeat the calculation", default=1)
+    
+    args = arg_parser.parse_args()
+    
+    start_time = time.time()
+
+    process_args = [args.upper_limit] * args.cores
+    #print("Process args -> ",process_args)
+    with Pool(processes=4) as pool:
+        pool.map(calculate_primes, process_args)
+    end_time = time.time()
+
+    print(f"The calculation took {end_time - start_time} seconds")
